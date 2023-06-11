d3d13b7cca4baee29008b8644c311499f433fe16
self479 <yiSiNing479@163.com>
2023-06-11 18:29:30 +0800

    整合td中的辅助数据和ti中的主要数据,

---
+++ src/main/java/tf/tf_order_act_d.scala
@@ -0,0 +1,110 @@
+package tf
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+
+/**
+  * 订单活动
+  * 2023/6/11
+  *
+  * @author 479
+  */
+object tf_order_act_d {
+  def main(args: Array[String]): Unit = {
+    System.setProperty("HADOOP_USER_NAME", "self479")
+    val spark = SparkSession.builder()
+      .appName("tf_order_act_d")
+      .master("local[*]")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    //处理day_id
+    var day_id = "20230502"
+    //获取昨天的日期
+    val yesterday = LocalDate.now.minus(1, ChronoUnit.DAYS) format DateTimeFormatter.ofPattern("yyyyMMdd")
+    //如果没有传参，则默认昨天
+   // if (args.length < 1) {
+   //   day_id = yesterday
+   // } else {
+   //   day_id = args(0)
+   // }
+    //平台活动
+    val plat = spark.sql(
+      s"""
+         |SELECT day_id,
+         |order_id,
+         |act_id,
+         |charge_fee
+         |FROM waimai.ti_order_plat_act_d
+         |where day_id=$day_id
+      """.stripMargin)
+
+
+    //商家活动
+    val poi = spark.sql(
+      s"""
+         |SELECT day_id,
+         |order_id,
+         |act_id,
+         |charge_fee
+         |FROM waimai.ti_order_plat_act_d
+         |where day_id=$day_id
+      """.stripMargin)
+
+
+
+    val actInfo = spark.sql(
+      s"""
+         |SELECT act_type_id,
+         |act_type_name,
+         |act_id,
+         |act_name
+         |FROM waimai.td_activity_info
+      """.stripMargin)
+    val act = plat.union(poi).join(actInfo, Seq("act_id"), "left_outer").distinct()
+
+    //设置hive的相关参数
+    spark.sqlContext.setConf("hive.exec.dynamic.partition", "true")
+    spark.sqlContext.setConf("hive.exec.dynamic.partition.mode", "nonstrict")
+    spark.sqlContext.setConf("hive.exec.max.dynamic.partitions", "10000")
+    spark.sqlContext.setConf("hive.exec.max.dynamic.partitions.pernoden", "1000")
+
+    val createTable =
+      """
+        |CREATE TABLE IF NOT EXISTS waimai.tf_order_act_d(
+        |order_id int,
+        |act_type_id int,
+        |act_type_name string,
+        |act_id int,
+        |act_name string,
+        |charge_fee double
+        |)
+        |COMMENT '订单活动'
+        |PARTITIONED BY (day_id string)
+        |STORED AS orc
+      """.stripMargin
+
+    println(createTable)
+    spark.sql(createTable)
+    act.createOrReplaceTempView("act")
+    //保存数据到目标表中
+    spark.sql(
+      s"""
+         |INSERT OVERWRITE TABLE waimai.tf_order_act_d partition(day_id)
+         |SELECT order_id,
+         |act_type_id,
+         |act_type_name,
+         |act_id,
+         |act_name,
+         |charge_fee,
+         |day_id
+         |FROM act
+         |WHERE day_id='$day_id'
+      """.stripMargin)
+
+    spark.stop()
+
+  }
+}
---
+++ src/main/java/tf/tf_poi_info_d.scala
@@ -0,0 +1,107 @@
+package tf
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * 商家信息
+  * 2023/06/11
+  *
+  * @author 479
+  */
+object tf_poi_info_d {
+  def main(args: Array[String]): Unit = {
+    System.setProperty("HADOOP_USER_NAME", "self479")
+    val spark = SparkSession.builder()
+      .appName("tf_poi_info_d")
+      .master("local[*]")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    //处理day_id
+    var day_id = "20230502"
+    //获取昨天的日期
+    val yesterday = LocalDate.now.minus(1, ChronoUnit.DAYS) format DateTimeFormatter.ofPattern("yyyyMMdd")
+    //如果没有传参，则默认昨天
+//    if (args.length < 1) {
+//      day_id = yesterday
+//    } else {
+//      day_id = args(0)
+//    }
+
+
+    val poi = spark.sql(
+      s"""
+         |SELECT poi_id,
+         |poi_name,
+         |city_id,
+         |phone,
+         |address,
+         |day_id
+         |FROM waimai.ti_poi_info_d
+         |WHERE day_id='$day_id'
+      """.stripMargin)
+
+    val city = spark.sql(
+      """
+        |SELECT province_id,
+        |province_name,
+        |city_id,
+        |city_name
+        |FROM waimai.td_city_info
+      """.stripMargin)
+
+    val poiDF = poi.join(city, Seq("city_id"), "left_outer")
+    poiDF.show(5)
+
+    //设置hive的相关参数
+    spark.sqlContext.setConf("hive.exec.dynamic.partition", "true")
+    spark.sqlContext.setConf("hive.exec.dynamic.partition.mode", "nonstrict")
+    spark.sqlContext.setConf("hive.exec.max.dynamic.partitions", "10000")
+    spark.sqlContext.setConf("hive.exec.max.dynamic.partitions.pernoden", "1000")
+
+    //spark.sql("DROP TABLE IF EXISTS waimai.tf_poi_info_d")
+
+    val createTable =
+      """
+        |CREATE TABLE IF NOT EXISTS waimai.tf_poi_info_d(
+        |poi_id int,
+        |poi_name string,
+        |province_id string,
+        |province_name string,
+        |city_id int,
+        |city_name string,
+        |phone string,
+        |address string)
+        |COMMENT '商家信息表'
+        |PARTITIONED BY (day_id string)
+        |STORED AS orc
+      """.stripMargin
+
+    println(createTable)
+    spark.sql(createTable)
+    poiDF.createOrReplaceTempView("poi")
+    //保存数据到目标表中
+    spark.sql(
+      s"""
+         |INSERT OVERWRITE TABLE waimai.tf_poi_info_d PARTITION(day_id)
+         |SELECT poi_id,
+         |poi_name,
+         |province_id,
+         |province_name,
+         |city_id,
+         |city_name,
+         |phone,
+         |address,
+         |day_id
+         |FROM poi
+         |WHERE day_id='$day_id'
+      """.stripMargin)
+
+    spark.stop()
+
+  }
+}
